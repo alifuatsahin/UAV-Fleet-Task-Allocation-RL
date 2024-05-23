df400b197a90479dd670e632c179916cfc739feb
alifuatsahin <alifuatpc@gmail.com>
2024-05-23 22:16:42 +0200

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -25,7 +25,7 @@ path = "logs/best_training/model.pt"
 
 agent.load_checkpoint(path)
 
-def baseline_strategy(state, uav_number):
+def baseline_strategy1(state, uav_number):
     attr_length = 10
     state = np.stack(state, axis=-1)
     lowest_healths = []
@@ -35,21 +35,25 @@ def baseline_strategy(state, uav_number):
         lowest_healths.append(uav_health.min(axis=0))
 
     action = th.softmax(th.tensor(lowest_healths), dim=0)
-    # action = th.tensor(np.ones(uav_number)/uav_number)
     return action
 
+def baseline_strategy2(state, uav_number):
 
+    action = th.tensor(np.ones(uav_number)/uav_number)
+    return action
 
 done1 = False
 done2 = False
 state1, _ = env1.reset()
 env2 = deepcopy(env1)
 state2 = env2._getObservation()
+env3 = deepcopy(env1)
+state3 = env3._getObservation()
 
 while not done1 or not done2:
 
     if not done1:
-        action1 = baseline_strategy(state1, 4)
+        action1 = baseline_strategy1(state1, 4)
         next_state1, reward1, terminated1, truncated1, info1 = env1.step(action1)
         done1 = terminated1 or truncated1
         state1 = next_state1
@@ -60,7 +64,14 @@ while not done1 or not done2:
         done2 = terminated2 or truncated2
         state2 = next_state2
 
+    if not done3:
+        action3 = baseline_strategy2(state3, 4)
+        next_state3, reward3, terminated3, truncated3, info3 = env3.step(action3)
+        done3 = terminated3 or truncated3
+        state3 = next_state3
+
 plt.figure()
-env1.plot_lowest_healths("Baseline")
+env1.plot_lowest_healths("Baseline - 1")
+env3.plot_lowest_healths("Baseline - 2")
 env2.plot_lowest_healths("Agent")
 plt.show()
